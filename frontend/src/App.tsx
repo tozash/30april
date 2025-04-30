b4c5360e37f34ecb10a6ada5b1343244163bb90d
girgvliani <nicklaki2003@gmail.com>
2025-04-30 21:21:06 +0400

    frontendTS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+//import PracticeView from "./components/PracticeView";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="app-title">Flashcard Learner Updated</h1>
+    </div>
+  );
+}
+
+export default App;
